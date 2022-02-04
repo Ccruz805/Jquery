d14a40c3085f9ede9ab49776c57c780dd14f65fb
Ccruz805 <63988491+Ccruz805@users.noreply.github.com>
2022-02-04 08:40:57 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	$('.showbtn1').click(function(){
+      $('.Answer1').hide();
+      $('.SSS').show();
+
+	});
+  	$('.hidebtn1').click(function(){
+      $('.SSS').hide();
+      $('.Answer1').show();
+
+	});
+});
+
+
+$(document).ready(function(){
+	$('.showbtn2').click(function(){
+      $('.Answer2').hide();
+      $('.ASA').show();
+
+	});
+  	$('.hidebtn2').click(function(){
+      $('.ASA').hide();
+      $('.Answer2').show();
+
+	});
+});
+
+$(document).ready(function(){
+	$('.showbtn3').click(function(){
+      $('.Answer3').hide();
+      $('.SAS').show();
+
+	});
+  	$('.hidebtn3').click(function(){
+      $('.SAS').hide();
+      $('.Answer3').show();
+
+	});
+});
